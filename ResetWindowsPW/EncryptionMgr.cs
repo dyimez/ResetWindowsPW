7396193c1c73c35fa52946714f442d361d370ecc
uyj <jimmy.uy@stramit.com.au>
2016-04-05 14:01:52 +1000

    Add project files.

---
+++ ResetWindowsPW/EncryptionMgr.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Security.Cryptography;
+
+namespace ResetWindowsPW
+{
+    class EncryptionMgr
+    {
+        private const string DEFAULT_KEY = "9!3Xc.g&UY@jst=zqAd+2md)w67";
+
+        public EncryptionMgr() { }
+        
+        /// <summary>
+        /// Encrypt a string.  The encrypted string can only be decrypted using the same key.
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="key"></param>
+        /// <returns> an encrypted byte[]</returns>
+        public string Encrypt(string plainText, string key)
+        {
+            string encryptedText = EncryptText(plainText, key);
+            return encryptedText;
+        }
+
+        /// <summary>
+        /// Encrypt a string using a default key.
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns> an encrypted byte[]</returns>
+        public string Encrypt(string plainText)
+        {
+            string encryptedText = EncryptText(plainText, DEFAULT_KEY);
+            return encryptedText;
+        }
+
+
+
+        /// <summary>
+        /// Decrypt an encrypted string. The encrypted string can only be decrypted using the same key 
+        /// that was for encryption.
+        /// </summary>
+        /// <param name="encryptedText"></param>
+        /// <param name="key"></param>
+        /// <returns> Decrypted string</returns>
+        public string Decrypt(string encryptedText, string key)
+        {
+
+
+            string decryptedText = DecryptText(encryptedText, key);
+            return decryptedText;
+        }
+
+
+        /// <summary>
+        /// Decrypt an encrypted string using default key. Use this method if string was encrypted using 
+        /// the default key.
+        /// </summary>
+        /// <param name="encryptedText"></param>
+        /// <returns> Decrypted string</returns>
+        public string Decrypt(string encryptedText)
+        {
+            string decryptedText = DecryptText(encryptedText, DEFAULT_KEY);
+            return decryptedText;
+        }
+
+
+        private string EncryptText(string plainText, string key)
+        {
+
+            try
+            {
+                byte[] encrypted;
+
+                using (TripleDESCryptoServiceProvider des = GetDES(key))
+                {
+                    byte[] buffPlainText = ASCIIEncoding.ASCII.GetBytes(plainText);
+                    encrypted = des.CreateEncryptor().TransformFinalBlock(buffPlainText, 0, buffPlainText.Length);
+                }
+                return Convert.ToBase64String(encrypted);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+
+        private string DecryptText(string encryptedText, string Key)
+        {
+
+            try
+            {
+                if (encryptedText.StartsWith("=") && encryptedText.Length <= 8)
+                {
+                    return encryptedText.Substring(1);
+                }
+                else
+                {
+                    byte[] decrypted;
+                    using (TripleDESCryptoServiceProvider des = GetDES(Key))
+                    {
+                        byte[] encryptedBuff = Convert.FromBase64String(encryptedText);
+                        decrypted = des.CreateDecryptor().TransformFinalBlock(encryptedBuff, 0, encryptedBuff.Length);
+                    }
+                    return ASCIIEncoding.ASCII.GetString(decrypted);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+
+        private TripleDESCryptoServiceProvider GetDES(string theKey)
+        {
+            TripleDESCryptoServiceProvider des;
+            MD5CryptoServiceProvider hashmd5;
+            byte[] pwdhash;
+
+            //create a secret password. the password (theKey) is used to encrypt
+            //and decrypt strings. Without the password, the encrypted
+            //string cannot be decrypted and is just garbage. You must
+            //use the same password to decrypt an encrypted string as the
+            //string was originally encrypted with.
+
+
+            //generate an MD5 hash from the password. 
+            //a hash is a one way encryption meaning once you generate
+            //the hash, you cant derive the password back from it.
+            hashmd5 = new MD5CryptoServiceProvider();
+            pwdhash = hashmd5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(theKey));
+            hashmd5 = null;
+
+            //implement DES3 encryption
+            des = new TripleDESCryptoServiceProvider();
+
+            //the key is the secret password hash.
+            des.Key = pwdhash;
+
+            //the mode is the block cipher mode which is basically the
+            //details of how the encryption will work. There are several
+            //kinds of ciphers available in DES3 and they all have benefits
+            //and drawbacks. Here the Electronic Codebook cipher is used
+            //which means that a given bit of text is always encrypted
+            //exactly the same when the same password is used.
+            des.Mode = CipherMode.ECB; //CBC, CFB
+            return des;
+        }
+
+    }
+}
---
+++ ResetWindowsPW/Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.DirectoryServices;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace ResetWindowsPW
+{
+    public partial class Main : Form
+    {
+        private string _username = string.Empty;
+        private string _password = string.Empty;
+        private string _domainname = string.Empty;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string suser = txtUser.Text;
+
+            if (string.IsNullOrEmpty(suser) == true)
+                return;
+
+
+            EncryptionMgr encryptionmgr = new EncryptionMgr();
+
+            //debug
+            string applicationName = Environment.GetCommandLineArgs()[0];
+
+            //live
+            //string applicationName = Environment.GetCommandLineArgs()[0] + ".exe";
+        
+            string exePath = System.IO.Path.Combine(Environment.CurrentDirectory, applicationName);
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+
+            string configusername = config.AppSettings.Settings["user"].Value.ToString();
+            string configpassword = config.AppSettings.Settings["password"].Value.ToString();
+            string configdomainname = config.AppSettings.Settings["domain"].Value.ToString();
+            string configisclosed = config.AppSettings.Settings["flag"].Value.ToString();
+
+            _username = encryptionmgr.Decrypt(configusername);
+            _password = encryptionmgr.Decrypt(configpassword);
+            _domainname = encryptionmgr.Decrypt(configdomainname);
+
+            //string sArg = " /C net user " + suser + " /domain /active:yes";
+            string sArg = " /" + configisclosed + " net user " + suser + " /domain /active:yes";
+
+            System.Security.SecureString ssPassword = new System.Security.SecureString();
+
+            for (int x = 0; x < _password.Length; x++)
+            {
+                ssPassword.AppendChar(_password[x]);
+            }
+
+            ProcessStartInfo psInfo = new ProcessStartInfo("cmd", sArg);
+
+            psInfo.CreateNoWindow = true;
+            psInfo.UserName = _username;
+            psInfo.Password = ssPassword;
+            psInfo.Domain = _domainname;
+            psInfo.UseShellExecute = false;
+
+            Process process = new Process();
+                
+            process.StartInfo = psInfo;
+            process.Start();
+        }
+    }
+}
